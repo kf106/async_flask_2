2b82a24fedcea1d34238b7f7f151e736098510da
kf106 <keir@chainfrog.com>
2017-10-08 15:50:53 +0300

    More comments in the code

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from random import random
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
+# Personally, I install and use eventlet
 async_mode = None
 
 app = Flask(__name__)
@@ -15,7 +16,8 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
-
+# The websocket is maintained in the background, and this
+# function outputs a random number every 5 seconds
 def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
@@ -27,10 +29,12 @@ def background_thread():
                       {'data': number, 'count': count},
                       namespace='/test')
 
+# Route for serving up the index page
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
+# This function is called when a web browser connects
 @socketio.on('connect', namespace='/test')
 def test_connect():
     global thread
@@ -39,13 +43,17 @@ def test_connect():
             thread = socketio.start_background_task(target=background_thread)
     emit('my_response', {'data': 'Connected', 'count': 0})
 
+# Ping-pong allows Javascript in the web page to calculate the
+# connection latency, averaging over time
 @socketio.on('my_ping', namespace='/test')
 def ping_pong():
     emit('my_pong')
 
+# Notification that a client has disconnected
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
+# Run the web app
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0')
